cc157bee63adcd50ffc6c57c5c8ff11cabc5c9e3
Suissa <jnascimento@gmail.com>
2017-02-25 08:41:54 -0300

    Subindo PoC

---
+++ actions/isOdd.js
@@ -0,0 +1,4 @@
+const fn = ( x ) => !( x % 2 )
+const signature = `Number -> Boolean`
+
+module.exports = { fn, signature }
---
+++ index.js
@@ -0,0 +1,7 @@
+const test = require('./testSignature')
+
+// const { fn, signature } = require('./actions/toDouble')
+const { fn, signature } = require('./actions/toString')
+// console.log('fn', fn)
+// console.log('signature', signature)
+console.log('test', test(signature, [4], fn))
