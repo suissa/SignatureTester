13a980b51954dd31250e481d6039ec0b453da11c
Lukas Wilkeer (@lukaswilkeer) <lukas4l@gmail.com>
2019-12-29 21:07:18 -0200

    fix: tests

--- test/signature.test.js
+++ test/signature.test.js
@@ -1,4 +1,10 @@
+const sinon = require('sinon')
 const { expect } = require('chai')
+const testSignature = require('../testSignature.js')
 
-describe.todo('Should parse the signature annotation')
-desctibe.todo('Should do in ou test on signatures')
+describe.skip('Should parse the signature annotation', () => {
+	it('should parse the annotation to types')
+	it('should parse every annotation to types')
+})
+
+describe.skip('Should do in ou test on signatures')
