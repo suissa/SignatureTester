7de02f2649d526f243bbd1d68d7f37e93e5ca473
Lukas Wilkeer (@lukaswilkeer) <lukas4l@gmail.com>
2020-03-14 09:36:20 -0300

    Removing old code

--- actions/fromArraytoObject.js
+++
@@ -1,7 +0,0 @@
-const toObject = ( acc, cur, i ) => 
-  Object.assign({}, acc, {[i]: cur})
-
-const fn = ( arr ) => arr.reduce ? arr.reduce( toObject, {} ) : arr
-const signature = `Array -> Object`
-
-module.exports = { fn, signature }
--- actions/isEven.js
+++
@@ -1,4 +0,0 @@
-const fn = ( x ) => !( x % 2 )
-const signature = `Number -> Boolean`
-
-module.exports = { fn, signature }
