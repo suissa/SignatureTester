2c316c36dac8e0d9b732150f8e37c5ab5134d86a
Lukas Wilkeer (@lukaswilkeer) <lukas4l@gmail.com>
2020-03-14 11:39:29 -0300

    fix: lib space and sinature pile

--- test/signature.test.js
+++ test/signature.test.js
@@ -1,10 +1,31 @@
 const sinon = require('sinon')
+const chai = require('chai')
 const { expect } = require('chai')
-const testSignature = require('../testSignature.js')
+const tester = require('../lib/index.js')
+const checkSignature = require('../lib/testSignature.js')
 
-describe.skip('Should parse the signature annotation', () => {
-	it('should parse the annotation to types')
-	it('should parse every annotation to types')
+describe('Signatures', () => {
+	it('should test the signature', (done) => {
+		const annotation = 'String -> String -> String'
+		const data = []
+		const fn = (str1, str2) => `${str1}.${str2}`
+
+		const result = checkSignature(annotation, data, fn)
+		expect(result._out).to.be.a('string')
+		done()
+	})
 })
 
-describe.skip('Should do in ou test on signatures')
+describe.skip('Should parse the signature annotation', () => {
+	it('should parse the annotation to types', (done) => {
+		const file = fs.readFileSync(`${process.cwd()}/test/__mocks__/description.js`)
+
+		const result = tester(file)
+
+		expect(result).to.be.an('array')
+		done()
+	})
+
+
+	it('should parse every annotation to types')
+})
